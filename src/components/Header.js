ce95112cedb3e9ce338d451f2a6ef53aca280e81
hagal <algir.s1996@gmail.com>
2023-02-24 16:01:51 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+export default function Header(props) {
+  return (
+    <header>
+      <nav className="teal lighten-1">
+        <div className="nav-wrapper container">
+          <a href="#" className="brand-logo">
+            ShopNumberOne
+          </a>
+          {/* <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Contacts</a>
+            </li>
+          </ul> */}
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = "d8ba7dcf-1347f8fb-ccae34ca-61a8bcc5";
+export const API_URL_LIST = "https://fortniteapi.io/v2/items/list?lang=ru";
+export const API_URL_ITEM = "https://fortniteapi.io/v2/items/get?lang=ru";
